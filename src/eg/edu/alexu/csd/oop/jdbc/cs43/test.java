8077a15de103f0fb23bc111bfdda2a89b838a7ad
mohamedabdelhakem1 <mohamedabdelhakem99@yahoo.com>
2018-12-11 21:38:33 +0300

    report

--- src/eg/edu/alexu/csd/oop/jdbc/cs43/test.java
+++ src/eg/edu/alexu/csd/oop/jdbc/cs43/test.java
@@ -20,7 +20,7 @@ import eg.edu.alexu.csd.oop.db.cs43.ExecuteStructureQuerys;
 public class test {
 
 	public static void main(String[] args) throws SQLException {
-		
+	/*	
 		Driver driver = new JDBCDriver();
 		Properties info =new Properties();
 		info.put("path",new File("C:\\Users\\SHIKO\\Desktop\\"+"sample" + System.getProperty("file.separator") +"database1"));
@@ -45,8 +45,8 @@ public class test {
 		while (set.previous()) {
 			System.out.println("value " +set.getInt(1));
 		}
-		*/
+		
 		statement.close();
-	}	
+	*/}	
 	
 }
